7b0b244efd420e199f0aa6d65b438e68e796ca8f
GIRIDHAR M A <82656662+giridhar-m-a@users.noreply.github.com>
2024-03-12 05:42:42 +0530

    updated config.json file with .env.local

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ LABEL version = "1.0"
 
 LABEL description = "IoT-backEnd application using nodeJS"
 
-WORKDIR /src/IoT-backEnd
+WORKDIR /src/IoT-Home-backEnd
 
 COPY package.json ./
 
@@ -16,8 +16,8 @@ RUN npm install
 
 COPY . .
 
-ENV IP 172.17.0.3
+#ENV IP 172.17.0.3
 
-EXPOSE 3000
+EXPOSE 3001
 
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,77 +1,72 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mqttPublish = require('./mqttPub');
-const mqttDetails = require('./mqttConfig.json')
-const mqtt = require('mqtt');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors middleware
+const mqttPublish = require("./mqttPub");
+const mqtt = require("mqtt");
+
 const app = express();
-const port = 3000;
+const port = 3001;
 
+// Enable CORS for all routes
+app.use(cors());
 
+// Parse JSON requests
+app.use(bodyParser.json());
 
-const mqttPort = mqttDetails.port;
-const mqttBroker = mqttDetails.mqttBroker;
-const mqttTopic = mqttDetails.mqttSubTopic;
-const mqttUser = mqttDetails.mqttUser;
-const mqttPassword = mqttDetails.mqttPassword;
-const client = mqtt.connect(mqttBroker,
-    {
-        port: mqttPort,
-        username: mqttUser,
-        password: mqttPassword,
-    });
-var sensorValues;
-
-
-
-client.on('connect', () => {
-    console.log('Connected to MQTT broker')
-    client.subscribe(mqttTopic)
-})
-
-client.on('message', (topic, message) => {
-    var currentTime = new Date();
-    var currentDate = new Date();
-    sensorValues = {
-        ...JSON.parse(message.toString()),
-        time: currentTime.toLocaleTimeString(),
-        date: currentDate.toLocaleDateString() // Add timestamp field as ISO string
-    };
-
-    console.log(sensorValues);
-
-
-})
-
-app.use(bodyParser.json())
-
-app.get('/sensorValues', async (req, res) => {
-    try {
-        res.json(sensorValues);
-    } catch (err) {
-        console.error(`Error retrieving sensor data: ${err}`);
-        res.status(500).json({ error: "Internal server error" });
-    } 
+// MQTT setup
+const mqttPort = process.env.port;
+const mqttBroker = process.env.mqttBroker;
+const mqttTopic = process.env.mqttSubTopic;
+const mqttUser = process.env.mqttUser;
+const mqttPassword = process.env.mqttPassword;
+const client = mqtt.connect(mqttBroker, {
+  port: mqttPort,
+  username: mqttUser,
+  password: mqttPassword,
 });
 
-app.post('/publish', (req, res) => {
-    try {
+let sensorValues;
 
-        const data = req.body;
-        console.log('Received data:', JSON.stringify(data));
-        mqttPublish.dataPublisher(data);
-        res.status(200).send('Data published to MQTT');
-
-    } catch (error) {
-        console.error('Error processing request:', error);
-        res.status(400).send('Invalid request data');
-    }
+client.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  client.subscribe(mqttTopic);
 });
 
+client.on("message", (topic, message) => {
+  var currentDateTime = new Date();
+  sensorValues = {
+    ...JSON.parse(message.toString()),
+    time: currentDateTime.toLocaleTimeString(),
+    date: currentDateTime.toLocaleDateString(), // Add timestamp field as ISO string
+  };
+
+  console.log(`sensor: ${JSON.stringify(sensorValues)}`);
+});
+
+// Route to get sensor values
+app.get("/sensorValues", async (req, res) => {
+  try {
+    res.json(sensorValues);
+  } catch (err) {
+    console.error(`Error retrieving sensor data: ${err}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Route to publish data to MQTT
+app.post("/publish", (req, res) => {
+  try {
+    const data = req.body;
+    console.log("Received data:", JSON.stringify(data));
+    mqttPublish.dataPublisher(data);
+    res.status(200).send("Data published to MQTT");
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(400).send("Invalid request data");
+  }
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
-
-
-
-
-
